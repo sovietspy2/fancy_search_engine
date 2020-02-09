981b989a77cf91e80d6fc6d1c24ab6190eafbf1d
Hang Barnabás <sovietspy2@yandex.com>
2020-02-09 21:37:06 +0100

    added sleep method

--- crawler/index.js
+++ crawler/index.js
@@ -4,14 +4,39 @@ const MongoClient = require("mongodb").MongoClient;
 // Connection URL
 const url = process.env.URL;
 
-// Database Name
-const dbName = "myproject";
-
 // Use connect method to connect to the server
-MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
-  console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+main();
 
-  client.close();
-});
+async function main() {
+  const db = await createConnection();
+
+  while ([] == ![]) {
+    // hehe
+    await wait(5000);
+    console.log("I waited");
+  }
+}
+
+async function wait(millis) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, millis);
+  });
+}
+
+async function createConnection() {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, function(
+      err,
+      client
+    ) {
+      if (err) {
+        reject(err);
+      }
+      console.log("Connected successfully to server");
+      resolve(client.db("index"));
+    });
+  });
+}
